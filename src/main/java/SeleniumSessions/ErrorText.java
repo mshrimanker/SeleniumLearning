9d04a7eac945bfd91b338b0c48992bb3dc099393
Mitesh Shrimanker <miteshshrimanker@gmail.com>
2020-07-27 23:04:47 +0100

    updated

--- src/main/java/SeleniumSessions/ErrorText.java
+++ src/main/java/SeleniumSessions/ErrorText.java
@@ -25,5 +25,7 @@ public class ErrorText {
 		
 		String errorText = driver.findElement(By.className("private-alert__title")).getText();
 		System.out.println(errorText);
+		
+		driver.quit();
 	}
 }
